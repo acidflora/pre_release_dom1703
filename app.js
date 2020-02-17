50784a29ac71058dfa70a0b9ed6bf451e260cd92
acidflora <ckeletoh@yandex.ru>
2020-02-18 00:32:28 +0300

    #2

--- app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("a[rel]").overlay(function() {
-    var wrap = this.getContent().find("div.wrap");
-    if (wrap.is(":empty")) {
-    wrap.load(this.getTrigger().attr("href"));
-    }
-    });
-    });
